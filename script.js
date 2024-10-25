262a2f0e639ab4536c368794b3aa66fee1f8564c
JP Brunache <34013885+kosmik7@users.noreply.github.com>
2024-10-26 00:23:07 +0200

    Add a range input to select the size of the canvas

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 
-function createCanvas() {
+function createCanvas(canvasSize = 16) {
+    const canvasContainer = document.getElementById('canvas-container');
     const canvasElement = document.getElementById('canvas');
-    const canvasSize = 16
+    const canvasElementNew = document.createElement('div')
+    canvasElementNew.setAttribute('id', 'canvas');
     const fragment = document.createDocumentFragment();
 
-    for (let i = 0; i <= canvasSize; i++) {
+
+
+    for (let i = 0; i < canvasSize; i++) {
         const canvasRow = document.createElement('div')
         canvasRow.classList.add('row')
 
-        for (let j = 0; j <= canvasSize; j++) {
+        for (let j = 0; j < canvasSize; j++) {
             const canvasPixel = document.createElement('div')
             canvasRow.appendChild(canvasPixel)
         }
-        fragment.appendChild(canvasRow);
+        canvasElementNew.appendChild(canvasRow);
     }
-    canvasElement.appendChild(fragment)
+    fragment.appendChild(canvasElementNew);
+    canvasContainer.replaceChild(fragment, canvasElement)
 }
+
+function updateLabel(canvasSize = 16) {
+    const canvasSizeLabel = document.getElementById('canvas-size-input-label')
+    canvasSizeLabel.textContent = `Canvas Size: ${canvasSize}x${canvasSize} `
+}
+
+const canvasSizeInput = document.getElementById('canvas-size-input')
+canvasSizeInput.addEventListener('mouseup', (event) => createCanvas(event.target.value));
+canvasSizeInput.addEventListener('input', (event) => updateLabel(event.target.value));
 
 createCanvas()
