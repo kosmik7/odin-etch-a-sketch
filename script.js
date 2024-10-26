45708a4488740deea7d5fb45b85aa42da2171d80
JP Brunache <34013885+kosmik7@users.noreply.github.com>
2024-10-26 11:04:39 +0200

    Add the rainbow mode option

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 
-function createCanvas(canvasSize = 16) {
+function createCanvas(canvasSize = canvasSettings.canvasSize) {
     const canvasElement = document.getElementById('canvas');
     const canvasElementNew = document.createElement('div')
     canvasElementNew.setAttribute('id', 'canvas');
     const fragment = document.createDocumentFragment();
-
-
 
     for (let i = 0; i < canvasSize; i++) {
         const canvasRow = document.createElement('div')
@@ -22,7 +20,7 @@ function createCanvas(canvasSize = 16) {
     canvasContainer.replaceChild(fragment, canvasElement)
 }
 
-function updateLabel(canvasSize = 16) {
+function updateLabel(canvasSize = canvasSettings.canvasSize) {
     const canvasSizeLabel = document.getElementById('canvas-size-input-label')
     canvasSizeLabel.textContent = `Canvas Size: ${canvasSize}x${canvasSize} `
 }
@@ -36,18 +34,44 @@ function trackMouse() {
 
 function drawPixel(event) {
     if (event.target.classList.contains('pixel')) {
-        event.target.style.backgroundColor = "black"
+        if (canvasSettings.rainbowMode) {
+            const randomHue = Math.floor(Math.random() * 360)
+            event.target.style.backgroundColor = `hsl(${randomHue} 80% 60%)`
+        } else {
+            event.target.style.backgroundColor = `black`
+        }
     }
 }
 
-const canvasSizeInput = document.getElementById('canvas-size-input')
-canvasSizeInput.addEventListener('mouseup', (event) => createCanvas(event.target.value));
-canvasSizeInput.addEventListener('input', (event) => updateLabel(event.target.value));
+function getSettings() {
+    const settingsContainer = document.getElementById('settings')
+    const canvasSizeInput = document.getElementById('canvas-size-input')
+
+    canvasSizeInput.addEventListener('mouseup', (event) => createCanvas(event.target.value));
+    settingsContainer.addEventListener('input', (event) => {
+        switch (event.target.id) {
+            case 'canvas-size-input':
+                updateLabel(event.target.value);
+                break;
+            case 'rainbow-mode-input':
+                canvasSettings.rainbowMode = event.target.checked
+                break;
+            default:
+        }
+    });
+}
+
+
+let canvasSettings = {
+    canvasSize: 16,
+    rainbowMode: false
+}
 
 const canvasContainer = document.getElementById('canvas-container');
 canvasContainer.addEventListener("mousedown", (event) => {
     event.preventDefault(); // prevents weird behavior when dragging mouse
     trackMouse();
-});
+});    
 
+getSettings()
 createCanvas()
