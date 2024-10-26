327c1357a4543f4ca0f440569045ce49c0b89834
JP Brunache <34013885+kosmik7@users.noreply.github.com>
2024-10-26 11:43:42 +0200

    Add a color picker mode

--- script.js
+++ script.js
@@ -34,45 +34,65 @@ function trackMouse() {
 
 function drawPixel(event) {
     if (event.target.classList.contains('pixel')) {
-        if (canvasSettings.rainbowMode) {
-            const randomHue = Math.floor(Math.random() * 360)
-            event.target.style.backgroundColor = `hsl(${randomHue} 80% 60%)`
-        } else if (canvasSettings.eraserMode) {
-            event.target.style.backgroundColor = `white`
-        } else {
-            event.target.style.backgroundColor = `black`
+        switch (true) {
+            case canvasSettings.colorMode:
+                event.target.style.backgroundColor = canvasSettings.color;
+                break;
+            case canvasSettings.rainbowMode:
+                const randomHue = Math.floor(Math.random() * 360);
+                event.target.style.backgroundColor = `hsl(${randomHue} 80% 60%)`;
+                break;
+            case canvasSettings.eraserMode:
+                event.target.style.backgroundColor = `white`;
+                break;
         }
     }
 }
 
 function getSettings() {
+    const settingsContainer = document.getElementById('settings')
+    const colorModeInput = document.getElementById('color-mode-input')
     const canvasSizeInput = document.getElementById('canvas-size-input')
     canvasSizeInput.addEventListener('input', (event) => {
         updateLabel(event.target.value);
     })
 
-    const settingsContainer = document.getElementById('settings')
     settingsContainer.addEventListener('change', (event) => {
         switch (event.target.id) {
             case 'canvas-size-input':
                 createCanvas(event.target.value);
                 break;
+            case 'color-input':
+                canvasSettings.color = event.target.value
+                colorModeInput.checked = true
+                canvasSettings.colorMode = true
+                canvasSettings.rainbowMode = false
+                canvasSettings.eraserMode = false
+                break;
+            case 'color-mode-input':
+                canvasSettings.colorMode = event.target.checked
+                canvasSettings.eraserMode = false
+                canvasSettings.rainbowMode = false
+                break;
             case 'rainbow-mode-input':
                 canvasSettings.rainbowMode = event.target.checked
                 canvasSettings.eraserMode = false
+                canvasSettings.colorMode = false
                 break;
             case 'eraser-mode-input':
                 canvasSettings.eraserMode = event.target.checked
                 canvasSettings.rainbowMode = false
+                canvasSettings.colorMode = false
                 break;
             default:
         }
     });
 }
 
-
 let canvasSettings = {
     canvasSize: 16,
+    color: '#000000',
+    colorMode: true,
     rainbowMode: false,
     eraserMode: false,
 }
@@ -81,7 +101,7 @@ const canvasContainer = document.getElementById('canvas-container');
 canvasContainer.addEventListener("mousedown", (event) => {
     event.preventDefault(); // prevents weird behavior when dragging mouse
     trackMouse();
-});    
+});
 
 getSettings()
 createCanvas()
